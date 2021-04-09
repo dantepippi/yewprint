d04c144c435037f6ec003b89b7ebe304a626a4d3
yozhgoor <yozhgoor@users.noreply.github.com>
2021-04-09 15:51:00 +0000

    deploy: 008d7e341bb91faa0590e76ae2af344b6b0a068b

--- implementors/yew_router/switch/trait.Switch.js
+++ implementors/yew_router/switch/trait.Switch.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["yew_router"] = [];
-implementors["yewprint_doc"] = [{"text":"impl Switch for DocMenu","synthetic":false,"types":[]}];
+implementors["yewprint_doc"] = [{"text":"impl <a class=\"trait\" href=\"yew_router/switch/trait.Switch.html\" title=\"trait yew_router::switch::Switch\">Switch</a> for <a class=\"enum\" href=\"yewprint_doc/enum.DocMenu.html\" title=\"enum yewprint_doc::DocMenu\">DocMenu</a>","synthetic":false,"types":["yewprint_doc::app::DocMenu"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
